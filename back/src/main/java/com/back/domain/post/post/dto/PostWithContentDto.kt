fe8427ae52ff272c174286247899be60a948d1c6
jhs512 <jangka512@gmail.com>
2026-07-16 11:59:18 +0900

    012

---
+++ back/src/main/java/com/back/domain/post/post/dto/PostWithContentDto.kt
@@ -0,0 +1,25 @@
+package com.back.domain.post.post.dto
+
+import com.back.domain.post.post.entity.Post
+import java.time.LocalDateTime
+
+@JvmRecord
+data class PostWithContentDto(
+    val id: Int,
+    val createDate: LocalDateTime,
+    val modifyDate: LocalDateTime,
+    val authorId: Int,
+    val authorName: String,
+    val title: String,
+    val content: String
+) {
+    constructor(post: Post) : this(
+        post.getId(),
+        post.getCreateDate(),
+        post.getModifyDate(),
+        post.getAuthor().getId(),
+        post.getAuthor().getName(),
+        post.getTitle(),
+        post.getContent()
+    )
+}
