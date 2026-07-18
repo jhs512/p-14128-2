b0f07cf16681f95af2e469e814a7c4d57d065578
jhs512 <jangka512@gmail.com>
2026-07-18 16:34:11 +0900

    014

---
+++ back/src/main/java/com/back/domain/member/member/dto/MemberDto.kt
@@ -0,0 +1,25 @@
+package com.back.domain.member.member.dto
+
+import com.back.domain.member.member.entity.Member
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDateTime
+
+@JvmRecord
+data class MemberDto(
+    val id: Int,
+    val createDate: LocalDateTime,
+    val modifyDate: LocalDateTime,
+    val name: String,
+    @get:JsonProperty("isAdmin")
+    val isAdmin: Boolean,
+    val profileImageUrl: String
+) {
+    constructor(member: Member) : this(
+        member.id,
+        member.createDate,
+        member.modifyDate,
+        member.name,
+        member.isAdmin,
+        member.profileImgUrlOrDefault
+    )
+}
